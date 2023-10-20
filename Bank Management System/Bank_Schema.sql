ce3e4f890998fe3d07490a12e8f60472de7c311b
SUCCESSIVE\akshat.kumar <akshat.kumar@successive.tech>
2023-10-20 11:54:22 +0530

    Adding all the tables in the Bank database and adding schema design also

---
+++ Bank Management System/Bank_Schema.sql
@@ -0,0 +1,54 @@
+use DB_BANK
+
+create table tbl_customers(
+	CustomerId int identity(1,1) primary key,
+	FirstName varchar(50),
+	LastName varchar(50),
+	Age int,
+	Date_of_birth date,
+	Cust_Address varchar(100),
+	MobileNo bigint
+)
+
+create table tbl_employees(
+	EmpId int identity(1,1) primary key,
+	FirstName varchar(50),
+	LastName varchar(50),
+	Age int,
+	Date_of_birth date,
+	Emp_Address varchar(100),
+	MobileNo bigint
+)
+
+create table tbl_accounts(
+	Acc_Number bigint primary key,
+	Acc_Type varchar(50),
+	Balance int,
+	Date_of_opening date,
+	Acc_Status varchar(50),
+	CustomerId int,
+	Foreign key (CustomerId) references tbl_customers(CustomerId)
+)
+
+create table tbl_transactions(
+	TransactionId int identity(1,1) primary key,
+	TransactionType varchar(50),
+	Amount int,
+	TransactionDate datetime,
+	Sender_Acc_Number bigint,
+	Receiver_Acc_Number bigint,
+	Foreign key (Sender_Acc_Number) references tbl_accounts(Acc_Number),
+	Foreign key (Receiver_Acc_Number) references tbl_Accounts(Acc_Number)
+)
+
+
+create table tbl_Loan(
+	LoanId int identity(1,1) primary key,
+	Amount int,
+	Interest_Rate float,
+	Time_In_Year int,
+	Starting_Date date,
+	Ending_Date date,
+	CustomerId int,
+	Foreign key(CustomerId) references tbl_customers(CustomerId)
+)
